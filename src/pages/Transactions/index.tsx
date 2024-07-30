75dd9271ff2b9708cab2162b2659af6c29d77dde
leonardo <leodsmf@gmail.com>
2024-07-30 16:17:22 -0300

    contexto de transacoes commit

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,32 +3,12 @@ import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { SearchForm } from "./components/SearchForm";
 import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
-  }
 
 export function Transactions() {
-
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-    }
-
-    useEffect(() => {
-    loadTransactions()
-    }, []);
-
+    const { transactions } = useContext(TransactionsContext);
   return (
     <div>
       <Header />
